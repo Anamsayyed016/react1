0d31d79133c9694b3b2ecb27cf222e25e95ffd66
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-09 02:56:00 +0530

    9febreac

--- Home.jsx
+++ Home.jsx
@@ -5,10 +5,43 @@ function Home(){
     return(
         <>
        <section>
-        <div>
+            <nav>
+        <div className="img-img-logo">
             <img src="logo.png" alt="" />
+
+               
         </div>
+        <div className="nav-nav-bar">
+        <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    
+                </ul>  
+                </div>
+        </nav>
        </section>
+
+            <section>
+                <div className="fir-fir-h">
+                    <h1>I Love Coding, Turning Ideas into Reality!!!!!!</h1>
+
+                    <h2>
+                        I am Anam Sayyed
+                    </h2>
+
+                    <h3>
+                        I am a Designer
+                    </h3>
+
+                        <h4>
+                        I am a Developer
+
+                        </h4>
+                </div>
+            </section>
         </>
         
     )
